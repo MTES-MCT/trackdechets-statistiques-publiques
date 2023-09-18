a4a8bb3c7f0f7444ec111c10082af7bda101f4a0
Laurent Paoletti <lp@providenz.fr>
2023-09-18 15:38:39 +0200

    Allow iframe embedding

--- src/config/settings/base.py
+++ src/config/settings/base.py
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 SITE_ID = env.int("SITE_ID", 1)
 
