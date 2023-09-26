b6d1af28c8fc3cfff5524db2f63fbea309e5a019
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2023-09-26 18:32:55 +0200

    First implementation of queries

---
+++ src/data/queries.py
@@ -0,0 +1,41 @@
+bs_weekly_data_sql = """
+select
+    *
+from
+    {}
+where
+    semaine >= '2020-01-01'
+"""
+
+accounts_weekly_stats_sql = """
+select
+    *
+from
+    refined_zone_stats_publiques.accounts_created_by_week
+where
+    semaine >= '2020-01-01'
+"""
+
+weekly_waste_processed_stats_sql = """
+select
+    *
+from
+    refined_zone_stats_publiques.weekly_quantity_processed_stats
+where
+    annee >= 2020
+"""
+
+
+accounts_by_naf_annual_stats_sql = """
+select
+    *
+from
+    refined_zone_stats_publiques.annual_company_accounts_created_by_naf
+"""
+
+waste_processed_by_naf_annual_stats_sql = """
+select
+    *
+from
+    refined_zone_stats_publiques.annual_waste_processed_by_naf
+"""
--- src/stats/processors/create_df.py
+++ src/stats/processors/create_df.py
@@ -23,3 +23,15 @@ def build_dataframes():
     bsd_data.all_bsds_data.write_json("temp_data/all_bsds_data.json")
     get_company_data().write_json("temp_data/company_data.json")
     get_user_data().write_json("temp_data/user_data.json")
+
+    for dataset_name in [
+        "bsdd_weekly_data",
+        "bsda_weekly_data",
+        "bsff_weekly_data",
+        "bsdasri_weekly_data",
+        "accounts_weekly_data",
+        "annual_waste_processed_data",
+        "accounts_by_naf_data",
+        "waste_processed_by_naf_annual_stats",
+    ]:
+        getattr(bsd_data, dataset_name).write_parquet(f"temp_data/{dataset_name}.parquet")
