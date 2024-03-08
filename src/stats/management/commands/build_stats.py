b6ee317293cb7ba43317418dcf72e0394e5097a4
Luis Ventura <30115537+Lawiss@users.noreply.github.com>
2024-03-08 12:44:31 +0100

    Adds summary stats for each bordereaux

--- src/stats/management/commands/build_stats.py
+++ src/stats/management/commands/build_stats.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 
 from ...processors.clear import clear_figs
 from ...processors.create_df import build_dataframes
-from ...processors.create_figs import build_figs
+from ...processors.stats_processor import build_stats_and_figs
 
 
 class Command(BaseCommand):
@@ -11,8 +11,8 @@ class Command(BaseCommand):
 
         clear_figs()
 
-        build_figs(2022, clear_year=True)
+        build_stats_and_figs(2022, clear_year=True)
 
-        build_figs(2023, clear_year=True)
+        build_stats_and_figs(2023, clear_year=True)
 
-        build_figs(2024, clear_year=True)
+        build_stats_and_figs(2024, clear_year=True)
