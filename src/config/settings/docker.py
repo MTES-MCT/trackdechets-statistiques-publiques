f43b8e3d3b8c82a82394e49cd29cb4f86b114a6e
Laurent Paoletti <lp@providenz.fr>
2024-04-04 11:44:47 +0200

    Dockerization

---
+++ src/config/settings/docker.py
@@ -0,0 +1,38 @@
+from .base import *  # noqa
+DATABASES = {}
+SECRET_KEY = "xyzabcdefghu"
+
+INSTALLED_APPS += [  # noqa F405
+    "whitenoise.runserver_nostatic",
+    "debug_toolbar",
+    "django_extensions",
+]
+
+
+ALLOWED_HOSTS = ["*"]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+MIDDLEWARE = (
+    MIDDLEWARE[:1]  # noqa
+    + [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
+    ]
+    + MIDDLEWARE[1:]  # noqa
+)
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+}
